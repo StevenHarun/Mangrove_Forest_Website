04e8c85a3f4156d52d8c2ec7379c4e7d1486ca5a
erisatrio1 <erisatrio03@gmail.com>
2024-05-06 22:01:10 +0700

    refactor : delete unnecessary code

--- resources/views/user/frontend/maps.blade.php
+++ resources/views/user/frontend/maps.blade.php
@@ -12,7 +12,7 @@
                     {{-- Tambahkan tombol registrasi --}}
                     {{-- @if (Route::has('register'))
                         < class="mt-4">
-                            <a href="{{ router') ="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            <a href="{{ router') ="rounded-md pxlack ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                             ister
                             </a>
                         <
