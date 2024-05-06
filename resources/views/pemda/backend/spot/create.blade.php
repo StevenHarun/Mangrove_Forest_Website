55c74b5c0785dc9d9791b3f1dd951f7e563064f5
erisatrio1 <erisatrio03@gmail.com>
2024-05-06 22:07:00 +0700

    refactor : delete unnecessary code

--- resources/views/pemda/backend/spot/create.blade.php
+++ resources/views/pemda/backend/spot/create.blade.php
@@ -68,7 +68,7 @@
 
                         <!-- fillColor -->
                         <!-- <div class="mt-4 grid grid-cols-2">
-                            <label for="fillColor" class="textFill Color</label>
+                            <label for="fillColortFill Color</label>
                             <x-text-input id="fis"b mt1w-mlClColor" />
                             <x-input-error :messages="$errors->get('fillColor')" class="mt-2" />
                         </div> -->
