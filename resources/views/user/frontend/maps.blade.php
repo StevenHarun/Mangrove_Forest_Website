479dc6f10463c96b406c653c42fdd94114d8fd5a
erisatrio1 <erisatrio03@gmail.com>
2024-05-06 22:00:54 +0700

    refactor : delete unnecessary code

--- resources/views/user/frontend/maps.blade.php
+++ resources/views/user/frontend/maps.blade.php
@@ -11,11 +11,11 @@
                 <!-- <div class="p-6 text-gray-900">
                     {{-- Tambahkan tombol registrasi --}}
                     {{-- @if (Route::has('register'))
-                        <div class="mt-4">
-                            <a href="{{ route('register') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                                Register
+                        < class="mt-4">
+                            <a href="{{ route('register') ="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            ister
                             </a>
-                        </div>
+                        <
                     @endif --}}
                 </div> -->
                 <div class="flex justify-between p-2">
