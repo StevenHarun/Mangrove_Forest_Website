ad5e7b3a713d89a7b4cd1f94b57a81e761ddc258
mutialifina <mutialifina@student.telkomuniversity.ac.id>
2024-05-08 13:42:01 +0700

    muti

---
+++ app/Models/reports.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class reports extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'report_title',
+        'category',
+        'location',
+        'coordinates',
+        'date',
+        'description',
+        'image'
+    ];
+}
