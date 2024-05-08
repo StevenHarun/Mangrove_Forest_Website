1c35935f69193fef20ec313466d427e1c5f83995
mutialifina <mutialifina@student.telkomuniversity.ac.id>
2024-05-08 13:53:08 +0700

    muti

---
+++ database/migrations/2024_05_02_072552_create_reports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('report_title');
+            $table->string('category');
+            $table->string('location');
+            $table->string('coordinates');
+            $table->date('date');
+            $table->text('description');
+            $table->timestamps();
+        });
+        DB::statement("ALTER TABLE reports ADD image LONGBLOB");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
