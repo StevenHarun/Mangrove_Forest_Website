21d54182f7e7ae0a94faf9ac7796b0b2442e2d70
Inka9403 <inkasharatu9@gmail.com>
2024-05-27 08:39:41 +0700

    Inka

--- resources/views/report/viewreport.blade.php
+++ resources/views/report/viewreport.blade.php
@@ -102,13 +102,14 @@
 
     <!-- Content -->
     <div class="py-6">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <!-- Header Table -->
-                    <div style="background-color: #75B896; color: white;" class="text-center py-3 mb-1 text-xl">
-                        Report History
-                    </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                <!-- Header Table -->
+                <div style="background-color: #75B896; color: white;" class="py-3 mb-1 text-xl font-bold pl-4">
+                    Report History
+                </div>
+
 
                     <!-- Filter Buttons -->
                     <div class="flex justify-center mt-4 ">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,28 +18,28 @@
 
 <body class="font-sans antialiased min-h-screen bg-gray-100">
     <!-- Kotak di atas latar belakang -->
-    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center z-10" style="background-color: #F2FAE9; width: 500px; height: 330px; border-radius: 30px;">
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-lg flex flex-col items-center z-10" style="background-color: #F2FAE9; width: 500px; padding: 20px; border-radius: 30px;">
         <!-- Logo -->
-        <div style="margin-top: -40px;">
-            <img src="{{ asset("images/Logo_Mangrove_Crop.png") }}" alt="Deskripsi gambar" style="width: 300px; height: auto;">
+        <div class="flex justify-center w-full">
+            <img src="{{ asset('images/Logo_Mangrove_Crop.png') }}" alt="Logo Mangrove Map" style="width: 250px; height: auto; margin-top: 10px;">
         </div> 
     
-        <!-- Space -->
-        <div style="margin-top: 20px;"></div>
-    
         <!-- Garis -->
-        <hr style="width: 85%; border: 1px solid grey; margin-top: 10px;">
+        <hr style="width: 85%; border: 1px solid grey; margin: 20px 0;">
     
-        <!-- Space -->
-        <div style="margin-top: 40px;"></div>
-    
+        <!-- Copywriting -->
+        <div class="text-justify mb-4 text-lg font-bold px-2">
+            <p>
+                Welcome to Mangrove Map! Dive into the wonders of our marine ecosystem and discover how we can protect our precious planet together. Ready to embark on this journey with us? Let's get started!
+            </p>
+        </div>
+        
         <!-- Tombol -->
-        <a href="{{ route('home') }}" class="bg-green-600 rounded-full px-8 py-3 font-bold">
+        <a href="{{ route('home') }}" class="bg-green-600 rounded-full px-8 py-3 font-bold mt-4">
             <span class="text-white">Let's Explore</span>
         </a>
     </div>
     
-
     <!-- Page Content -->
     @include('layouts.navigation_all')
     <!-- Page Heading -->
